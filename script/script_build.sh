e7d0cc97a00520428339e97dfddf8bd8e33e33ef
Gothdroid Team <gothdroid@gothdroid.com>
2015-04-26 14:52:56 +0200

    Create script_build.sh

---
+++ script/script_build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export PATH=/data/bin:$PATH
+export USE_CCACHE=1
+export CCACHE_DIR=/data/ccache/jenkins/ValidusRom
+export VALIDUS_BUILDTYPE=OFFICIAL
+export KBUILD_BUILD_USER=Gothdroid
+export KBUILD_BUILD_HOST=Gothdroid
+
+cd /data/jenkins/workspace/ValidusRom
+
+prebuilts/misc/linux-x86/ccache/ccache -M 60G
+
+make clean
+
+. build/envsetup.sh
+lunch validus_$device-userdebug
+make validus -j8
