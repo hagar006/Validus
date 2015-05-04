155f297ea4d7ecfb77eae5c623fd6fbd8d69690b
Gothdroid Team <gothdroid@gothdroid.com>
2015-05-04 07:32:15 +0200

    test delete all local manifest before pull

--- script/update.sh
+++ script/update.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+# Different export
 export PATH=/data/bin:$PATH
+
+# Remove all local_manifests in folder
+$file = *.xml
+$chemin = /data/jenkins/workspace/ValidusRom/.repo/local_manifests/
+
+if [ -f $chemin/$file ]; then
+  rm -rf $file
+fi
 
 cd /data/jenkins/workspace/ValidusRom
 git pull && repo sync -j4
