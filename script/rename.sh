f6da708dab634c168ad91e5200981499c0ca9acd
Gothdroid Team <gothdroid@gothdroid.com>
2015-06-13 16:18:52 +0200

    add rename.sh

---
+++ script/rename.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+HOME=/data/jenkins/workspace/ValidusRom
+DEVICE=$1
+VERSION=$2
+DAY=$(date +%Y%m%d)
+
+cd $HOME/out/target/product/$DEVICE
+
+mv validus-*.zip Validus-Build-$VERSION-$DAY.zip
