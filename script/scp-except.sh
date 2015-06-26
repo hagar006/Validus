cc06d8eae4a461d2305d76d0e9709d4f2d88c2e5
Gothdroid <gothdroid@gothdroid.com>
2015-06-26 11:44:59 +0200

    Update scp-except.sh

--- script/scp-except.sh
+++ script/scp-except.sh
@@ -1,1 +1,13 @@
 #!/usr/bin/expect -f
+
+$DEVICE=$1
+
+# effectue le transfert par SCP (sur SSH)
+
+set timeout 3600
+
+spawn scp /data/opendelta/publish/$DEVICE/* gothdroid@gothdroid.com:/data/opendelta/publish/$DEVICE/
+expect "password:"
+send -- "AmyLee$33450\r"
+expect "100%"
+exit 0
