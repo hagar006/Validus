1a4f386aa58e14504beaf848d14f657b283ce26b
Gothdroid Team <gothdroid@gothdroid.com>
2015-06-30 15:29:16 +0200

    fix port :(

--- script/sftp.sh
+++ script/sftp.sh
@@ -9,7 +9,7 @@ UPDATE=Validus-*.update
 	echo -e "Starting transfert"
 	cd /data/opendelta/publish/$DEVICE
 	
-	sftp -oPort=5112 -oIdentityFile=/data/jenkins/.ssh/id_dsa gothdroid@gothdroid.com <<EOF
+	sftp -oPort=5212 -oIdentityFile=/data/jenkins/.ssh/id_dsa gothdroid@gothdroid.com <<EOF
 	cd /data/opendelta/publish/$DEVICE
 	put *
 	bye
