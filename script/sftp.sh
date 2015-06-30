6bae7babefaec2e96c0fc4bd574946e35cdeff13
Gothdroid Team <gothdroid@gothdroid.com>
2015-06-30 16:37:26 +0200

    suppress unused variable

--- script/sftp.sh
+++ script/sftp.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 DEVICE=$1
-ZIP=Validus-*.zip
-DELTA=Validus-*.delta
-SIGN=Validus-*.sign
-UPDATE=Validus-*.update
 	
 	echo -e "Starting transfert"
 	cd /data/opendelta/publish/$DEVICE
