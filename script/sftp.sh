7ec890ca7ffc4fa5e86451ab5ac6f51764dc2c38
Gothdroid Team <gothdroid@gothdroid.com>
2015-06-30 12:37:32 +0200

    modify

--- script/sftp.sh
+++ script/sftp.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
 
-DEVICE='$1'
-ZIP='Validus-*.zip'
-DELTA='Validus-*.delta'
-SIGN='Validus-*.sign'
-UPDATE='Validus-*.update'
+DEVICE=$1
+ZIP=Validus-*.zip
+DELTA=Validus-*.delta
+SIGN=Validus-*.sign
+UPDATE=Validus-*.update
 	
 	echo -e "Starting transfert"
-	
-	/usr/bin/sftp -oPORT=5212 -oIdentityFile=/data/jenkins/.ssh/id_dsa gothdroid@gothdroid.com <<EOF	
-	lcd /data/opendelta/last/$DEVICE
-	cd /data/opendelta/last/$DEVICE
-	rm -rf $ZIP
-	put $ZIP
-	lcd /data/opendelta/publish/$DEVICE
 	cd /data/opendelta/publish/$DEVICE
-	put $DELTA
-	put $SIGN
-	put $UPDATE
 	
+	lftp<<END_SCRIPT
+	open sftp://gothdroid@gothdroid.com
+	cd /data/opendelta/publish/$DEVICE
+	mput *
 	bye
-	bye
-	EOF
+	`END_SCRIPT'
 	
-
+	
 	
