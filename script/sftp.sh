afbbe81ec29098b89b0a5f61122e088196a1ede7
Gothdroid Team <gothdroid@gothdroid.com>
2015-06-30 14:29:41 +0200

    fix

--- script/sftp.sh
+++ script/sftp.sh
@@ -9,9 +9,10 @@ UPDATE=Validus-*.update
 	echo -e "Starting transfert"
 	cd /data/opendelta/publish/$DEVICE
 	
-	lftp<<END_SCRIPT
+	lftp<< END_SCRIPT
 	open sftp://gothdroid.com:5112
-	user gothdroid AmyLee$33450
+	user gothdroid
+	pass AmyLee$33450
 	cd /data/opendelta/publish/$DEVICE
 	mput *
 	bye
