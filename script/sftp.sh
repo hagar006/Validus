049e862b891a843adcc49ea2c99033f31b6f9bc3
Gothdroid Team <gothdroid@gothdroid.com>
2015-06-30 12:04:11 +0200

    fix sftp script fo test

--- script/sftp.sh
+++ script/sftp.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
 
-cd $HOME2/last/$DEVICE
+DEVICE='$1'
+ZIP='Validus-*.zip'
+DELTA='Validus-*.delta'
+SIGN='Validus-*.sign'
+UPDATE='Validus-*.update'
 	
 	echo -e "Starting transfert"
 	
-	/usr/bin/sftp -oPORT=5212 -oIdentityFile=/data/jenkins/.ssh/id_dsa gothdroid@gothdroid.com << EOF	
+	/usr/bin/sftp -oPORT=5212 -oIdentityFile=/data/jenkins/.ssh/id_dsa gothdroid@gothdroid.com <<EOF	
 	lcd /data/opendelta/last/$DEVICE
 	cd /data/opendelta/last/$DEVICE
-	rm -rf *.zip
-	put *.zip
+	rm -rf $ZIP
+	put $ZIP
+	lcd /data/opendelta/publish/$DEVICE
+	cd /data/opendelta/publish/$DEVICE
+	put $DELTA
+	put $SIGN
+	put $UPDATE
+	
 	bye
 	bye
 	EOF
+	
 
 	
