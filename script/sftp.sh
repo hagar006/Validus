472b60275513e785c3ac44521cbd079849d744fc
Gothdroid Team <gothdroid@gothdroid.com>
2015-06-30 18:56:07 +0200

    add build transfer

--- script/sftp.sh
+++ script/sftp.sh
@@ -8,5 +8,11 @@ DEVICE=$1
 	sftp -oPort=5212 -oIdentityFile=/data/jenkins/.ssh/id_dsa gothdroid@gothdroid.com <<EOF
 	cd /data/opendelta/publish/$DEVICE
 	put *
+	ls
+	lcd /data/opendelta/last/$DEVICE
+	cd /data/opendelta/last/$DEVICE
+	rm *
+	put *
+	ls
 	bye
 	EOF
